ba4484f04d1d857b789a651baff05553db9d463e
wangliqiu <wangliqiu@niwodai.net>
2018-01-22 14:35:47 +0800

    ddd

--- eureka/src/main/java/com/wangliqiu/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/wangliqiu/eureka/EurekaApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 控制台：http://localhost:8761/
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
@@ -11,4 +14,6 @@ public class EurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
+
+
 }

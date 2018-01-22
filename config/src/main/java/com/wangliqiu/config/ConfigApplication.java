402c4996130f4e2ffed7fe9a98be4f9302729d53
wangliqiu <wangliqiu@niwodai.net>
2018-01-22 18:07:34 +0800

    ddd

--- config/src/main/java/com/wangliqiu/config/ConfigApplication.java
+++ config/src/main/java/com/wangliqiu/config/ConfigApplication.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 请求：
+ * /{application}/{profile}[/{label}]
+ * 映射：
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {

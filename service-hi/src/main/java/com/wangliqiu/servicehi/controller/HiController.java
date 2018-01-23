4599315e01ba273d87ee8c846e919fb1625623ef
wangliqiu <wangliqiu@niwodai.net>
2018-01-23 16:38:35 +0800

    ddd

--- service-hi/src/main/java/com/wangliqiu/servicehi/controller/HiController.java
+++ service-hi/src/main/java/com/wangliqiu/servicehi/controller/HiController.java
@@ -1,7 +1,6 @@
 package com.wangliqiu.servicehi.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 // RefreshRemoteApplicationEvent 会触发HiController新实例
-@RefreshScope
+//@RefreshScope  // todo 报错了？？？
 public class HiController {
 
 	@Value("${server.port}")
 	String port;
 
+	// todo 报错了？？？
 	/* 从配置中心获取 */
-	@Value("${foo}")
-	String foo;
+//	@Value("${foo}")
+//	String foo;
 
 
 	@RequestMapping("/hi")
-	public String hi(@RequestParam String name) {
+	public String hi(@RequestParam String name, @Value("${foo}") String foo) {
 		return "hi " + name + ": " + port + " -> " + foo;
 	}
 

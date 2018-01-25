c17a931151bdbd3aacdaf7660393b88ffcb30662
wangliqiu <wangliqiu@niwodai.net>
2018-01-25 19:19:18 +0800

    ddd

--- hystrix-dashboard/src/main/java/com/wangliqiu/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/wangliqiu/hystrixdashboard/HystrixDashboardApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  * 该Servlet用来跨域访问event stream, 地址：localhost:9999/proxy.stream?origin=xxx:xxx/hystrix.stream
  * <p>
  * 注意：如果dashboard配置在服务节点，则监控自己节点就没反应，因为ProxyStreamServlet的跨域头？？？
+ * 注意：服务节点的hystrix不被调用就不发送消息？所以一开始不调用服务节点，dashboard就卡住。
  */
 @EnableHystrixDashboard
 @SpringBootApplication

76eae77b42f2d08cf0b329c49f11524fb6efeadf
wangliqiu <wangliqiu@niwodai.net>
2018-01-25 19:20:04 +0800

    ddd

--- hystrix-turbine/src/main/java/com/wangliqiu/hystrixturbine/HystrixTurbineApplication.java
+++ hystrix-turbine/src/main/java/com/wangliqiu/hystrixturbine/HystrixTurbineApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+/**
+ * todo 聚合只会显示第一个？？？
+ */
 // 包含@EnableDiscoveryClient功能，从注册中心获取
 @EnableTurbine
 @SpringBootApplication
